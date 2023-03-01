ef1a9936780955886f61304890087c4533c30253
Elbish <E.Bhusal.inf21@stud.bbbaden.ch>
2023-03-01 08:09:28 +0100

    server

--- server/app.js
+++ server/app.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static("../client/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
