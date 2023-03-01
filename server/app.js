01681bec4619f9865139a86bef9cb08baf229140
Elbish <E.Bhusal.inf21@stud.bbbaden.ch>
2023-03-01 10:20:38 +0100

    server edited

--- server/app.js
+++ server/app.js
@@ -10,18 +10,23 @@ app.use(express.static("../client/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
-
 // ========================================= API ===================================================
 
 app.get("/menu/getAll", menuController.getAll);
 
 // == LOGIN/REGISTER ==
 
+app.get("/login", function (req, res) {
+  const { username, password } = req.body.username;
+
+  res.send(username, password);
+});
 app.post("/login", loginController.login);
 app.post("/auth", loginController.auth);
 app.post("/changepassword", loginController.changepassword);
 app.post("/register", loginController.register);
 app.get("/getuserlist", loginController.getuserlist);
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
